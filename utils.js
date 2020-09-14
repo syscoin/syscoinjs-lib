fa4e9e5148d37a2a42cf61196ac7e2b53f337fe9
sidhujag <jsidhu@blockchainfoundry.co>
2020-09-14 15:39:35 -0700

    move psbt code into response section

--- utils.js
+++ utils.js
@@ -66,13 +66,13 @@ async function fetchBackendAsset (backendURL, assetGuid) {
 
 async function sendRawTransaction (backendURL, txHex, myHDSignerObj) {
   try {
-    const psbt = bjs.Psbt.fromHex(txHex)
-    if (psbt instanceof bjs.Psbt === false) {
-      throw new Error('PSBT could not be decoded from hex')
-    }
     const request = await axios.post(backendURL + '/api/v2/sendtx/', txHex)
     if (request && request.data) {
       if (myHDSignerObj) {
+        const psbt = bjs.Psbt.fromHex(txHex, { network: myHDSignerObj.network })
+        if (psbt instanceof bjs.Psbt === false) {
+          throw new Error('PSBT could not be decoded from hex')
+        }
         await fetchBackendTxs(backendURL, myHDSignerObj.getAccountXpub(), 'tokens=used&details=tokens', true, myHDSignerObj)
       }
       return request.data
