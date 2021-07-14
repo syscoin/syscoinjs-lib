31eac9ceffcc2982f705f12dd7e81d33ddd9163d
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2021-07-13 18:29:58 -0700

    update erc20mgr

--- utils.js
+++ utils.js
@@ -418,8 +418,8 @@ Returns: Returns JSON object in response, SPV proof object in JSON
 */
 async function buildEthProof (assetOpts) {
   const ethProof = new GetProof(assetOpts.web3url)
-  const ERC20ManagerTestnet = '0x58a628D012E76263031ef8DBF1d75e9CdA216671'
-  const ERC20ManagerMainnet = '0x58a628D012E76263031ef8DBF1d75e9CdA216671'
+  const ERC20ManagerTestnet = '0xDB9254d17a8cAAB186ad5bE76B8D00a05E960F2B'
+  const ERC20ManagerMainnet = '0xDB9254d17a8cAAB186ad5bE76B8D00a05E960F2B'
   const tokenFreezeFunction = '7ca654cf9212e4c3cf0164a529dd6159fc71113f867d0b09fdeb10aa65780732' // token freeze function signature
   try {
     let result = await ethProof.transactionProof(assetOpts.ethtxid)
